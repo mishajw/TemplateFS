9becf33a104ba1c51689c0e23cd72d5d19caafbf
Misha Wagner <mishajw@gmail.com>
2016-08-06 11:16:39 +0200

    Clean up varible fetchingand make url function return dict

--- variable_fetch.py
+++ variable_fetch.py
@@ -3,10 +3,17 @@
 import os
 from urllib import parse
 
+def get_all_variables():
+    pass
+
 def get_env():
     return os.environ;
 
+
 def get_from_url(url):
+    return dict(_get_from_url_generator(url))
+
+def _get_from_url_generator(url):
     params_section = url[url.index("?") + 1:]
     params_text = params_section.split("&")
     
@@ -19,11 +26,11 @@ def get_from_url(url):
 
         yield _url_decode(param_split[0]), _url_decode(param_split[1])
 
-
 def _url_decode(url):
     return parse.unquote(url)
 
 params = get_from_url("index?a=x&b=y&c=z")
+print(params["b"])
 
 for p in params:
     print(p)
